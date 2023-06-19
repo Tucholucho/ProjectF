44fc49efd5b8c1c4d94c2ec730723ed23b85a7c7
Tucholucho <lucholp17@gmail.com>
2023-06-19 11:12:38 -0300

    DB code

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,10 +3,12 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+  sequelize.define(
+    'recipe', 
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
   });
 };
