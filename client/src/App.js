246cf256e51fe7d62f8a93cfc627cc801d6a1d44
Tucholucho <lucholp17@gmail.com>
2023-06-21 20:30:25 -0300

    Terminado, pero faltan retoques de css

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import "./App.css"
-import {useState, useEffect} from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 import {
   Route,
   Routes,
@@ -7,7 +7,7 @@ import {
   useLocation,
   useSearchParams,
 } from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getAllRecipes,
   getDailyRecipes,
@@ -18,14 +18,14 @@ import {
 } from "./redux/actions";
 import Nav from "./components/Nav/Nav";
 import Landing from "./components/Landing/Landing";
-import Render from "./components/Cards/Render";
+import Renderer from "./components/Cards/Render";
 import Detail from "./components/Cards/Detail";
 import Form from "./components/CreateForm/Form";
 import About from "./components/About/About";
+import axios from 'axios'
 
-const baseURL = "http://localhost:3001";
 
-function App(){
+function App() {
   const dispatch = useDispatch();
   const location = useLocation();
   const navigate = useNavigate();
@@ -33,19 +33,17 @@ function App(){
   const dailyRecipes = useSelector((state) => state.filteredDailyRecipes);
 
   const [loading, setLoading] = useState(false);
-  const [diets,setDiets] = useState([]);
+  const [diets, setDiets] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams({});
   const [currentPage, setCurrentPage] = useState(1);
-  const [search, setSearch] = useState ("");
-  const [orderBy, setOrderBy] = useState ({
-    by: searchParams.get("by")|| "Alphabetical",
-    order: searchParams.get("order")||"Ascendant",
+  const [search, setSearch] = useState("");
+  const [orderBy, setOrderBy] = useState({
+    by: searchParams.get("by") || "Alphabetical",
+    order: searchParams.get("order") || "Ascendant",
   });
-  const [filterByDiet, setFilterByDiet] = useState(
-    searchParams.get("filter") || "all"
-  );
+  const [filterByDiet, setFilterByDiet] = useState(searchParams.get("filter") || "All");
 
-  const recipesPerPage = 9;
+  const recipesPerPage = 9; // este estado local setea cuantas cartas entran por pagina
   const indexOfLastRecipe = currentPage * recipesPerPage;
   const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
   const currentRecipe = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
@@ -54,79 +52,73 @@ function App(){
     let search = searchParams.get("search");
     let page = searchParams.get("page");
     let by = searchParams.get("by");
-    let order = searchParams("order");
+    let order = searchParams.get("order");
     let filter = searchParams.get("filter");
 
     if (search) {
       setSearch(search);
       onSearch(search);
     }
-    if (page){
-      paginator(page,"num");
+    if (page) {
+      paginator(page, "num");
     }
-    orderCards({...orderBy, by, order});
+    orderCards({...orderBy, by, order });
     filterRecipes(filter);
+  }, []);
 
-  }, []); //Que escuche los cambios url
-
-
+  // Modifica la query segun el estado.
   useEffect(() => {
-    let by = orderBy.by ||"Alphabetical";
+    let by = orderBy.by || "Alphabetical";
     let order = orderBy.order || "Ascendant";
     let searchParam = search || "";
     let filter = filterByDiet || "All";
     setSearchParams({
       search: searchParam,
-      pare: currentPage,
+      page: currentPage,
       filter,
       by,
       order,
     });
-  }, [search,currentPage, orderBy,filterByDiet]);
+  }, [search, currentPage, orderBy, filterByDiet]);
 
   useEffect(() => {
-    fetch(`${baseURL}/recipes/all`)
-    .then((response) => response.json())
-    .then ((data) => {
-      let dailyMeals = [];
+    axios.get(`/recipes/all`)
+      .then((response) => {
+        let dailyMeals = [];
+        let breakfast = response.data.find((r) => r.dishTypes.includes("Breakfast"));
+        let lunch =
+          response.data.find((r) => r.dishTypes.includes("Lunch") && r !== breakfast) ||
+          response.data.find((r) => r.dishTypes.includes("Lunch"));
+        let brunch =
+          response.data.find(
+            (r) =>
+              r.dishTypes.includes("Brunch") && r !== breakfast && r !== lunch
+          ) || response.data.find((r) => r.dishTypes.includes("Brunch"));
+        let dinner =
+          response.data.find(
+            (r) =>
+              r.dishTypes.includes("Dinner") &&
+              r !== breakfast &&
+              r !== lunch &&
+              r !== brunch
+          ) || response.data.find((r) => r.dishTypes.includes("Dinner"));
 
-      let breakfast = data.find((r) => r.dishTypes.includes("Breakfast"));
-      
-      let lunch = 
-        data.find((r) => r.dishTypes.includes("Lunch") && r !== breakfast) ||
-        data.find((r) => r.dishTypes.includes("Lunch"));
-        
-      let brunch = data.find(
-        (r) =>
-            r.dishTypes.includes("Brunch") && r !== breakfast && r !== lunch
-        ) || data.find((r) => r.dishTypes.includes("Brunch"));
-      
-      let dinner = 
-      data.find(
-        (r) =>
-          r.dishTypes.includes("Dinner") &&
-          r !== breakfast &&
-          r !== lunch &&
-          r !== brunch
-      ) || data.find((r) => r.dishTypes.includes("Dinner"));
+        dailyMeals.push(breakfast, lunch, brunch, dinner);
 
-      dailyMeals.push(breakfast, lunch, brunch, dinner);
-
-      dispatch(getDailyRecipes(dailyMeals));
-    });
+        dispatch(getDailyRecipes(dailyMeals));
+      });
   }, [dispatch]);
 
   useEffect(() => {
-    fetch(`${baseURL}/diet/`)
-      .then((response) => response.json())
-      .then((data) => {
-        data.sort((a, b) => {
+    axios.get("/diet/")
+      .then((response) => {
+        response.data.sort((a, b) => {
           if (a < b) return -1;
           if (a > b) return 1;
           return 0;
         });
-        data.unshift("All");
-        setDiets(data);
+        response.data.unshift("All");
+        setDiets(response.data);
       });
     return () => {
       setDiets([]);
@@ -165,46 +157,39 @@ function App(){
 
   async function createRecipe(userData) {
     setLoading(true);
-    const response = await fetch(`${baseURL}/recipes`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    });
-    let data = await response.json();
+    const response = await axios.post("/recipes", userData);
     setLoading(false);
-    return window.alert(data.res);
+    console.log(response.data)
+    return window.alert(response.data.res);
   }
 
   async function onSearch(name) {
     setLoading(true);
-    await fetch(`${baseURL}/recipes?name=${name}`)
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch(getAllRecipes(data));
+    await axios.get(`/recipes?name=${name}`)
+      .then((response) => {
+        dispatch(getAllRecipes(response.data));
         setLoading(false);
       });
   }
 
-  function filterRecipes(filter){
+  function filterRecipes(filter) {
     if (filter === "All") dispatch(unfilterRecipe());
-    else if (filter !== "All") dispatch (filterRecipe(filter));
+    else if (filter !== "All") dispatch(filterRecipe(filter));
   }
 
   function orderCards(order) {
     let orderedRecipes = [];
     let orderedDailyRecipes = [];
-    if (order.by === "Alphabetical"){
-      orderedRecipes =[...recipes].sort((a,b)=>
-        sortTitle(a,b, order.order)
+    if (order.by === "Alphabetical") {
+      orderedRecipes = [...recipes].sort((a, b) =>
+        sortTitle(a, b, order.order)
       );
-      orderedDailyRecipes = [...dailyRecipes].sort((a,b) =>
-      sortTitle(a,b,order.order)
+      orderedDailyRecipes = [...dailyRecipes].sort((a, b) =>
+        sortTitle(a, b, order.order)
       );
     } else if (order.by === "HealthScore") {
-      orderedRecipes = [...recipes].sort((a,b) =>
-      sortHealthScore(a,b,order.order)
+      orderedRecipes = [...recipes].sort((a, b) =>
+        sortHealthScore(a, b, order.order)
       );
       orderedDailyRecipes = [...dailyRecipes].sort((a, b) =>
         sortHealthScore(a, b, order.order)
@@ -215,35 +200,35 @@ function App(){
       dispatch(orderDailyRecipes(orderedDailyRecipes));
   }
 
-  function sortTitle (a,b,order){
+  function sortTitle(a, b, order) {
     const titleA = a.title.toUpperCase();
     const titleB = b.title.toUpperCase();
 
     if (order === "Ascendant") {
       if (titleA < titleB) return -1;
-      if (titleA > titleB) return +1;
+      if (titleA > titleB) return 1;
       return 0;
     } else if (order === "Descendant") {
       if (titleB < titleA) return -1;
-      if (titleB > titleA) return -1;
+      if (titleB > titleA) return 1;
       return 0;
     }
   }
 
-  function sortHealthScore(a,b,order){
-    if (order==="Ascendant") return a.healthScore - b.healthScore;
-    else if (order === "Descendant") return b.healthScore - a.healtScore;
-  } 
+  function sortHealthScore(a, b, order) {
+    if (order === "Ascendant") return a.healthScore - b.healthScore;
+    else if (order === "Descendant") return b.healthScore - a.healthScore;
+  }
 
   return (
     <div className="App">
-      {location.pathname !== "/" && <Nav/>}
+      {location.pathname !== "/" && <Nav />}
       <Routes>
-        <Route path="/" element={<Landing/>} />
-        <Route 
+        <Route path="/" element={<Landing />} />
+        <Route
           path="/home"
           element={
-            <Render
+            <Renderer
               recipes={recipes}
               onSearch={onSearch}
               goToRecipeCreator={goToRecipeCreator}
@@ -258,13 +243,13 @@ function App(){
               recipesPerPage={recipesPerPage}
               paginator={paginator}
               currentPage={currentPage}
-            /> 
-        }
-      />
-      <Route path="/recipes/:recipeId" element={<Detail />} />
-      <Route
-        path="/createRecipe"
-        element={<Form createRecipe={createRecipe} loading={loading} />}
+            />
+          }
+        />
+        <Route path="/recipes/:recipeId" element={<Detail />} />
+        <Route
+          path="/createRecipe"
+          element={<Form createRecipe={createRecipe} loading={loading} />}
         />
         <Route path="/about" element={<About />} />
       </Routes>
